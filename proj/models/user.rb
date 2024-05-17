483f113798aa3320b066708ee58cc4dfc5d37350
palaciosmica12 <palaciosmica12@gmail.com>
2024-05-17 00:44:02 -0300

    Actualización proj con errores en db

--- proj/models/user.rb
+++ proj/models/user.rb
@@ -3,9 +3,10 @@
 require 'bcrypt'
 
 class User < ActiveRecord::Base
-  has_secure_password
+  include BCrypt
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
+
 end
