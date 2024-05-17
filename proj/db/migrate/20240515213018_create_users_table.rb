95bd00c9032dd4c120a31e73d47683cbb2d189b1
palaciosmica12 <palaciosmica12@gmail.com>
2024-05-17 01:03:23 -0300

    Actualización proj - Sin error de rb

--- proj/db/migrate/20240515213018_create_users_table.rb
+++ proj/db/migrate/20240515213018_create_users_table.rb
@@ -1,11 +1,10 @@
 class CreateUsersTable < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username, null: false
-      t.string :email, null: false
-      t.string :password, null: false
+      t.string :username
+      t.string :email
+      t.string :password
 
     end
   end
-
 end
