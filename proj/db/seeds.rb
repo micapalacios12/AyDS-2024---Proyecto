7447521885c365d18cb18e7045e4aae2bc81f3cb
Marysol Gutierrez <gutimary88@gmail.com>
2024-10-01 21:49:23 -0300

    sacando del seed los user cargados y eliminando de la base de datos usuarios creados que le faltaban la columna level_completd

--- proj/db/seeds.rb
+++ proj/db/seeds.rb
@@ -3,28 +3,6 @@ require_relative '../models/question'
 
 require_relative '../models/option'
 
-
-# Define una lista de usuarios con sus atributos
-users = [
-  { names: 'Jon Doe', username: 'jondoe', email: 'jon@doe.com', password: 'password123' },
-  { names: 'Jane Doe', username: 'janedoe', email: 'jane@doe.com', password: 'password123' },
-  { names: 'Baby Doe', username: 'babydoe', email: 'baby@doe.com', password: 'password123' }
-]
-
-# Itera sobre cada usuario en la lista
-users.each do |u|
-  unless User.exists?(username: u[:username]) || User.exists?(email: u[:email])
-    # Crea un nuevo usuario en la base de datos utilizando los atributos proporcionados
-    User.create!(
-      names: u[:names],
-      username: u[:username],
-      email: u[:email],
-      password: u[:password],
-      password_confirmation: u[:password]
-    )
-  end
-end
-
 # Eliminar las preguntas y opciones existentes para evitar duplicados
 Question.destroy_all
 Option.destroy_all
